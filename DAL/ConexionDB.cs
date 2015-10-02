831353f7e07c446851b559bdf091273961a0ebad
Melvin0496 <melvingabriel65@gmail.com>
2015-10-02 12:04:02 -0400

    Agregando metodo ejecutar y obtener datos en la clase conexionDb

--- DAL/ConexionDB.cs
+++ DAL/ConexionDB.cs
@@ -4,19 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace DAL
 {
     
-    class ConexionDB
+    public class ConexionDB
     {
         private SqlConnection con;
         private SqlCommand cmd;
 
         public ConexionDB()
         {
-            con = new SqlConnection("Data Source = JUNIOR-PC\ROOT;Initial Catalog = RegistroPeliculaDb;Integrated Security = true");
+            con = new SqlConnection("Data Source = JUNIOR-PC\\ROOT;Initial Catalog = RegistroPeliculaDb;Integrated Security = true");
             cmd = new SqlCommand();
+        }
+        public bool Ejecutar(String ComandoSql)
+        {
+            bool retorno = false;
+
+            try
+            {
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = ComandoSql;
+                cmd.ExecuteNonQuery();
+                retorno = true;
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return retorno;
+        }
+        public DataTable ObtenerDatos(string ComandoSql)
+        {
+            SqlDataAdapter adapter;
+            DataTable dt = new DataTable();
+
+            try
+            {
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = ComandoSql;
+
+                adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dt);
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return dt;
         }
     }
 }
