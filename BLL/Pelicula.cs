bdef56be1ab300f20f8ac9ddb4666b670406a373
Melvin0496 <melvingabriel65@gmail.com>
2015-10-02 11:16:52 -0400

    Agregando los constructores de la clase pelicula

--- BLL/Pelicula.cs
+++ BLL/Pelicula.cs
@@ -8,5 +8,33 @@ namespace BLL
 {
     class Pelicula
     {
+        public int PeliculaId { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int Ano { get; set; }
+        public int Calificacion { get; set; }
+        public int IMDB { get; set; }
+        public int CategoriaId { get; set; }
+
+        public Pelicula()
+        {
+            this.PeliculaId = 0;
+            this.Titulo = "";
+            this.Descripcion = "";
+            this.Ano = 0;
+            this.Calificacion = 0;
+            this.IMDB = 0;
+            this.CategoriaId = 0;
+        }
+        public Pelicula(int pPeliculaId, string pTitulo, string pDescripcion,int pAno,int pCalificacion,int pIMDB,int pCategoriaId)
+        {
+            PeliculaId = pPeliculaId;
+            Titulo = pTitulo;
+            Descripcion = pDescripcion;
+            Ano = pAno;
+            Calificacion = pCalificacion;
+            IMDB = pIMDB;
+            CategoriaId = pCategoriaId;
+        }
     }
 }
